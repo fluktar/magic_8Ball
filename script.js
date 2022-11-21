f53b57fbc5086acfc63b12ac86cec2702cee8ebc
fluktar <tyc.pawel@gmail.com>
2022-11-21 14:12:43 +0100

    8Ball_fin update

--- script.js
+++ script.js
@@ -10,18 +10,30 @@ let answersArr = [
 	'Ciężko powiedzieć...',
 	'Nie chcesz znać odpowiedzi na to pytanie... :/',
 ];
+const shakeBall = () => {
+    ball.classList.add('shake-animation')
+   setTimeout(checInput,1000)
+}
+const checInput = () => {
+	if (input.value !== '' && input.value.slice(-1) === '?') {
+		generateAnswer();
+		pError.textContent = '';
+        ball.classList.remove('shake-animation')
+	} else if (input.value !== '' && input.value.slice(-1) !== '?') {
+        pError.textContent = 'Pytanie musi być zakończone znakiem "?".';
+		pAnswer.textContent = '';
+        ball.classList.remove('shake-animation')
+	}else{
+        pError.textContent = 'Musisz zadać jakieś pytanie!';
+		pAnswer.textContent = '';
+        ball.classList.remove('shake-animation')
 
-
-
-
-
-
-
-
+    }
+};
 
 const generateAnswer = () => {
 	const number = Math.floor(Math.random() * answersArr.length);
 	pAnswer.innerHTML = `<span>Odpowiedź:</span> ${answersArr[number]}`;
 };
 
-ball.addEventListener('click', generateAnswer);
+ball.addEventListener('click', shakeBall);
