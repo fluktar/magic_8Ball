bb51ca641b547d429c5b20f125851f6b2f8791a5
fluktar <tyc.pawel@gmail.com>
2022-11-21 13:41:03 +0100

    update8Ball

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
-const ball = document.querySelector('img')
-const pAnswer = document.querySelector('.answer')
-const pError = document.querySelector('.error')
-const input = document.querySelector('input')
+const ball = document.querySelector('img');
+const pAnswer = document.querySelector('.answer');
+const pError = document.querySelector('.error');
+const input = document.querySelector('input');
 
-const answersArr = ['Tak!','Nie.', 'Może.', 'Ciężko powiedzieć...', 'Nie chcesz znać odpowiedzi na to pytanie... :/']
+let answersArr = [
+	'Tak!',
+	'Nie.',
+	'Może.',
+	'Ciężko powiedzieć...',
+	'Nie chcesz znać odpowiedzi na to pytanie... :/',
+];
+
+
+
+
+
+
+
+
 
 const generateAnswer = () => {
-    
-}
+	const number = Math.floor(Math.random() * answersArr.length);
+	pAnswer.innerHTML = `<span>Odpowiedź:</span> ${answersArr[number]}`;
+};
 
-ball.addEventListener('click',generateAnswer)
-
+ball.addEventListener('click', generateAnswer);
