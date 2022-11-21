e5c5cbdfba47c0dcec3b0a0e1b4bd6230c23eebe
fluktar <tyc.pawel@gmail.com>
2022-11-21 13:07:46 +0100

    update

---
+++ script.js
@@ -0,0 +1,13 @@
+const ball = document.querySelector('img')
+const pAnswer = document.querySelector('.answer')
+const pError = document.querySelector('.error')
+const input = document.querySelector('input')
+
+const answersArr = ['Tak!','Nie.', 'Może.', 'Ciężko powiedzieć...', 'Nie chcesz znać odpowiedzi na to pytanie... :/']
+
+const generateAnswer = () => {
+    
+}
+
+ball.addEventListener('click',generateAnswer)
+
